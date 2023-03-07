c5d357b60277a4675d764ac088da640fe1907e8c
Musa Molla <musamolla3131@gmail.com>
2023-03-07 11:37:15 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Virtual-Function/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Animal {
+public:
+  virtual void sound() { cout << "Aminal is making a sound." << endl; }
+};
+
+class Dog : public Animal {
+public:
+  void sound() override { cout << "Dog is barking." << endl; }
+};
+
+class Cat : public Animal {
+public:
+  void sound() override { cout << "Cat is meowing." << endl; }
+};
+
+int main() {
+  Animal *a = new Dog();
+  a->sound();
+  a = new Cat();
+  a->sound();
+  return 0;
+}
